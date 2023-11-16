7ee1fe535b6b1a9f0acb4d6eddf9f68c2912a217
Sigurd Humerfelt <shumerf@eu.trimblecorp.net>
2023-11-16 10:55:43 +0100

    model name param

--- LEVERINGSMAPPE/model.py
+++ LEVERINGSMAPPE/model.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 import numpy as np
 
 class model:
-    def __init__(self, path):
-        self.model = tf.keras.models.load_model(os.path.join(path, 'training_1_SK_BRANN_CL_15_21'))
-
+    def __init__(self, path, modelname="model32"):
+        self.model = tf.keras.models.load_model(os.path.join(path, modelname))
+        #'training_1_SK_BRANN_CL_15_21'
     def predict(self, X):
         
         # Note: this is just an example.
